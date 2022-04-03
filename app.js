0f02a132ab355aa7100428bf51110a6bbc6f365a
MarceloLimaCodes <marcelolima557@gmail.com>
2022-04-03 00:01:23 -0300

    5º commit: Remoção de linhas desnecessárias + Correção de bug verSenha homepage

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ btnConfirm.addEventListener('click', () => {
 
 
 // Campo com evento que muda a cor do label e input quando atinge uma quantidade específica de caracteres.
-// É repetida usando a variável usuario, email, senha e confirmar senha.
+// É repetida para usuario, email, senha e confirmar senha.
 
 usuario.addEventListener('keyup', () => {
     if(usuario.value.length <= 4){
@@ -112,10 +112,7 @@ senha.addEventListener('keyup', () => {
     }
 })
 
-// CAMPO CONFIRM SENHA:
-
-// se os caracteres do valor de senha forem diferentes dos caracteres do valor da confirmação de senha ele cai no seu "if" informando
-// que sua condição é falsa, alterando sua cor para vermelho e ela não passará pelo "else" que mudará a cor do label para verde
+// CONFIRMAR SENHA:
 
 confirmSenha.addEventListener('keyup', () => {
     if(senha.value != confirmSenha.value){
@@ -143,14 +140,8 @@ confirmSenha.addEventListener('keyup', () => {
 
 // BOTÃO CADASTRAR:
 
-// O codigo abaixo é a função cadastrar do botão, que pega a informação das condições de cima (se ele fica verde ou se ele fica vermelho
-// acontece algo). Se essas coisas não acontecerem, eles não passam pelo 'if' da função cadastrar que foram validados pelo true
-// e false, que são responsáveis por enviar os valores do imput para dentro do localStorage.
-// se todas cairem no "true" essa informação é validada e passada para o banco, caso alguma seja falsa, ela cai no "else" que não
-// deixa a sua operação terminar e imprime uma mensagem de erro vermelha no formato HTML (função innerHTML).
-
-function cadastrar() {   // assim que clicamos bate aqui, todas originalmene são cadastradas nas variáveis como falsas
-    if(validUsuario && validMail && validSenha && validConfirmSenha) { // o if so funciona se todas forem verdadeiras
+function cadastrar() {
+    if(validUsuario && validMail && validSenha && validConfirmSenha) { 
 
         let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
 
@@ -165,10 +156,8 @@ function cadastrar() {   // assim que clicamos bate aqui, todas originalmene sã
 
         localStorage.setItem('listaUser', JSON.stringify(listaUser))
 
-        // mensagem de cadastrando usuario definindo(set) o atributo(Attribute) style do html que interfere(style) no css
         msgSucess.setAttribute('style', 'display: block')
-        msgSucess.innerHTML = ('<strong>Cadastrando usuário...<strong/>')//insere no html
-        msgError.innerHTML = ''
+        msgSucess.innerHTML = ('<strong>Cadastrando usuário...<strong/>')
         msgError.setAttribute('style', 'display: none')
 
         setTimeout(()=> {
@@ -180,10 +169,11 @@ function cadastrar() {   // assim que clicamos bate aqui, todas originalmene sã
     } else {
         msgError.setAttribute('style', 'display: block')
         msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
-        msgSucess.innerHTML = ''
         msgSucess.setAttribute('style', 'display: none')
     }
 }
+
+// BOTÃO ENTRAR:
 
 function entrar () {
     let usuario = document.querySelector('#usuario')
@@ -203,7 +193,8 @@ function entrar () {
 
     listaUser = JSON.parse(localStorage.getItem('listaUser'))
 
-    // console.log(listaUser) para ver os objetos JSON da variavel 'listaUser' que já está cadastrado, para essa nova variável listaUser
+    // console.log(listaUser) para ver os objetos JSON de listaUser que está cadastrado no localStorage 
+    // utilizando essa nova variável listaUser
 
     listaUser.forEach((item) => {
         if(usuario.value == item.userCad && senha.value == item.senhaCad) {
@@ -212,30 +203,26 @@ function entrar () {
                 mail: item.mailCad,
                 senha: item.senhaCad
             }
+
+            window.location.href = 'logado.html'
+
+            let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2)
+            localStorage.setItem('token', token)
+    
+            localStorage.setItem('userLogado', JSON.stringify(userValid))
+
+        } else {
+            usuario.setAttribute('style', 'border-color: red')
+            userLabel.setAttribute('style', 'color:red')
+            userLabel.innerHTML = 'Usuário (Preencha o campo corretamente)'
+            senha.setAttribute('style', 'border-color: red')
+            senhaLabel.setAttribute('style', 'color: red')
+            senhaLabel.innerHTML = 'Senha (Preencha o campo corretamente)'
+            msgError.innerHTML = 'Usuário e senha não conferem'
+            msgError.setAttribute('style', 'display: block')
+            usuario.focus()
         }
     })
 
- // console.log(userValid) literalmente da mesma forma que foi escrito esse método: "para cada item(forEach item) se o valor digitado no
- // input usuario for igual ao valor usuario do LocalStorage e(&&) a mesma coisa para senha, o userValid vai retornar os itens
- // do localStorage.
 
-    if(usuario.value == userValid.user && senha.value == userValid.senha) {
-        window.location.href = 'logado.html'
-
-        let token = Math.random().toString(16).substr(2) + Math.random().toString(16).substr(2)
-        localStorage.setItem('token', token)
-
-        localStorage.setItem('userLogado', JSON.stringify(userValid))
-
-    } else {
-        usuario.setAttribute('style', 'border-color: red')
-        userLabel.setAttribute('style', 'color:red')
-        userLabel.innerHTML = 'Usuário (Preencha o campo corretamente)'
-        senha.setAttribute('style', 'border-color: red')
-        senhaLabel.setAttribute('style', 'color: red')
-        senhaLabel.innerHTML = 'Senha (Preencha o campo corretamente)'
-        msgError.innerHTML = 'Usuário e senha não conferem'
-        msgError.setAttribute('style', 'display: block')
-        usuario.focus()
-    }
 }
